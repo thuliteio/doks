3482846159caec61d1377a4a9264e60f12d67c18
Henk Verlinde <henk@ventizo.com>
2020-11-05 16:19:35 +0100

    config: update csp

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,20 +13,6 @@ if (localStorage.getItem('theme') === 'dark') {
 
 }
 
-/* eslint-disable */
-/*
-window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
-
-if (document.querySelector('#deploy-to-netlify') !== null) {
-
-  document.getElementById('deploy-to-netlify').addEventListener('click', function(){
-    plausible('Deploy to Netlify');
-  });
-
-}
-*/
-/* eslint-enable */
-
 var suggestions = document.getElementById('suggestions');
 var userinput = document.getElementById('userinput');
 

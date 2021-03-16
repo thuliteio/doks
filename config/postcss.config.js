253b938a2e303a06d07488c6eae79c74442e56d6
Henk Verlinde <henk@ventizo.com>
2021-03-16 15:14:45 +0100

    feat: add katex math typesetting

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -23,6 +23,7 @@ module.exports = {
           './assets/scss/components/_code.scss',
           './assets/scss/components/_search.scss',
           './assets/scss/common/_dark.scss',
+          './node_modules/katex/dist/katex.css'
         ]),
       ],
     }),

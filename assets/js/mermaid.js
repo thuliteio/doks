6a87dc6e29d0675316ae912bc96a26ae7b368c1a
Henk Verlinde <henk@ventizo.com>
2021-07-01 08:48:56 +0200

    feat: update script loading

--- assets/js/mermaid.js
+++ assets/js/mermaid.js
@@ -1,3 +1,5 @@
+import mermaid from 'mermaid/dist/mermaid';
+
 var config = {
   theme: 'default',
   fontFamily: '"Jost", -apple-system, blinkmacsystemfont, "Segoe UI", roboto, "Helvetica Neue", arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',

fa01f4ab56541a5acc954f04cc48c67ffa2772ee
James Elliott <james-d-elliott@users.noreply.github.com>
2022-06-25 12:20:57 +1000

    fix: avoid error if unset

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,7 +95,7 @@ Source:
   // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
 
   {{ $list := slice }}
-  {{- if eq (len .Site.Params.options.searchSectionsIndex) 0 }}
+  {{- if or (not (isset .Site.Params.options "searchsectionsindex")) (eq (len .Site.Params.options.searchSectionsIndex) 0) }}
   {{- $list = (where .Site.Pages "Section" "docs") }}
   {{- else }}
   {{- $list = (where .Site.Pages "Type" "in" .Site.Params.options.searchSectionsIndex) }}
